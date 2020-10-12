9d12c3148272eb5acaeb4d5dd8ac27407c460723
nodreamistoobig <46135817+nodreamistoobig@users.noreply.github.com>
2020-10-12 08:49:18 +0800

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,36 +1,29 @@
+import java.util.ArrayList
+
 fun add (a: Double, b:Double): Double = a+b
 fun sub (a: Double, b:Double): Double = a-b
 fun div (a: Double, b:Double): Double = a/b
 fun mul (a: Double, b:Double): Double = a*b
 
-fun isNumber(s:String):Boolean{
-    if (s.isEmpty()) return false
-    for (symbol in s){
-        if (!symbol.isDigit()){
-            return false
-        }
-    }
-    return true
-}
-
 val opers = arrayOf("+", "-", "*", "/")
 var maxDeapth = 0
 var lastIndex = 0
+var expr = ArrayList<String>();
 
 fun operation(oprd: String, index: Int, parts: List<String>, deapth: Int): Double{
-    var op1 = 0.0
-    var op2 = 0.0
+    var op1: Double
+    var op2: Double
     if (deapth>maxDeapth)
         maxDeapth = deapth
 
     if (parts[index+1] in opers){
-        print("(")
+        expr.add("(");
         op1 = operation(parts[index+1], index+1, parts, deapth+1)
-        print(")")
+        expr.add(")");
     }
     else if(parts[index+1].toDouble()>=0){
         op1 = parts[index+1].toDouble()
-        print("$op1")
+        expr.add(op1.toString());
     }
     else {
         println("Wrong expression")
@@ -38,13 +31,15 @@ fun operation(oprd: String, index: Int, parts: List<String>, deapth: Int): Doubl
     }
 
     if (parts[index+2 + (maxDeapth-deapth)*2] in opers){
-        print(" $oprd (")
+        expr.add(oprd);
+        expr.add("(");
         op2 = operation(parts[index+2+ (maxDeapth-deapth)*2], index+2+ (maxDeapth-deapth)*2, parts, deapth+1)
-        print(")")
+        expr.add(")");
     }
     else if(parts[index+2+ (maxDeapth-deapth)*2].toDouble() >= 0){
         op2 = parts[index+2+ (maxDeapth-deapth)*2].toDouble()
-        print(" $oprd $op2")
+        expr.add(oprd);
+        expr.add(op2.toString());
     }
     else {
         println("Wrong expression")
@@ -64,20 +59,22 @@ fun operation(oprd: String, index: Int, parts: List<String>, deapth: Int): Doubl
 }
 
 
-fun main(args: Array<String>) {
+fun main() {
 
     println("Input expression")
     val answer: String? = readLine()
     var parts = answer?.split(' ')
-    var stack1 = mutableListOf<String>()
-    val stack2 = mutableListOf<String>()
     try {
         if (parts != null) {
             var result = operation(parts[0], lastIndex, parts, 0)
             if (lastIndex < parts.size)
                 println("Wrong expression")
-            else
-                println(" = $result")
+            else{
+                for (part in expr){
+                    print("$part ")
+                }
+                println("= $result")
+            }
         }
     }
     catch (e: Exception){
